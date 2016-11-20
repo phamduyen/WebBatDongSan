633691ff5e091f993ef01e857f875d1817c62d29
Vuongdoan12 <thegioi9x0000@gmail.com>
2016-11-20 11:11:42 +0700

    Du an

--- App_Data/Models/models.generated.cs
+++ App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "6e7fc1461184ca4")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
